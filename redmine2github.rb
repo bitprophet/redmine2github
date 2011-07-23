e0aad290305b3470711ba068d0a8fc9014da90f1
Jeff Forcier <jeff@bitprophet.org>
2011-07-23 18:12:49 -0400

    Workflow skeleton comments

--- redmine2github.rb
+++ redmine2github.rb
@@ -40,7 +40,27 @@ Issue.find(:all, :order => "id ASC").each do |issue|
     skipped_ids << issue.id
     next
   rescue RestClient::ResourceNotFound
-    puts "not on gh"
-    exit if issue.id > 5
+    # Create new issue based on mapping, and:
+    #   Submitter note in desc
+    #   Assigned to me
+    #   Create date in desc
+    # If issue was closed, close it on GH
+    # Set labels for "dupe", "wontfix" etc
+    # Set labels for quick, wart, others?
+    # For each attachment:
+    #   If not text in nature, warn & skip
+    #   Create gist
+    #   Add note at bottom of desc w/ link
+    # For each related issue:
+    #   Add note+link at bottom of desc
+    # For each journal/comment, sorting by created_on:
+    #   Add to GH issue
+    #   Include original username, submit date in body field
+    # Assign to appropriate milestone:
+    #   If closed, assign to real closed milestone
+    #   If open, label as 1.x or 2.x - no milestone
+    break if issue.id > 5
   end
 end
+
+puts "Skipped #{skipped_ids.size} issue(s) numbered #{skipped_ids.join(", ")}."
